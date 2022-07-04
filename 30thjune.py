bb0a182066ac909a14afc86a4b2c07e872bf5984
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-07-04 21:21:41 +0530

    Add files via upload

---
+++ 30thjune.py
@@ -0,0 +1,50 @@
+# Single Line Comment
+
+"""
+Multi
+Line Comments
+"""
+
+'''
+Multi 
+Line
+Comments
+'''
+# print("Hello World")
+# print('HEllo World')
+
+
+print("Hello World",end="\t")
+print("Hello World")
+
+print("Hello World",end=" ")
+print("Hello world")
+
+print("Hello World",end="HI")
+print("Hello World")
+
+print("Hello World",end="")
+print("Hello")
+
+"""
+\n ---> new line
+\t ---> tab
+\b ---> backspace
+\r ---> carriage return
+"""
+
+# print("How are \b you?")
+# print("Python is an interpreted language\rAnaconda")
+
+"""
+Hello\namaste\thank you\bye
+"""
+
+print("Hello\\namaste\ \bthank you\ \bbye")
+
+"""
+\t is used to give tab but \\t is used to print \t
+"""
+"""
+Alpha said Beta:"Gamma is teasing Delta, but gamma said 'I didn't tease delta, I tease Theta'"
+"""
---
+++ 4thJuly.py
@@ -0,0 +1,151 @@
+"""
+Numeric DataTypes:-->
+---------------------------------
+1) Integer
+2) Complex Numbers
+3) Float
+"""
+
+# a = 10
+
+# print(type(a))
+# print(a)
+
+# b = 128977985451321549841233212313213218784545454
+# print(type(b))
+# print(b)
+
+# c = 25.52
+# print(type(c))
+
+# d = 15623.645549842123132184512131
+# print(d)
+
+# e = 4 + 5j
+
+# print(type(e))
+# # print(e)
+
+# a = True
+# print(type(a))
+
+# b = False
+# print(type(b))
+
+"""
+Arithmetic Operators:
+
+1) +
+2) - 
+3) *
+4) /
+5) %
+6) // (floor division)
+7) ** (exponent)
+"""
+# a = 2
+# b = 5
+
+# print("Division: ",a/b)
+# print("Floor Division: ",a//b)
+# print("Exponent: ",a**b)
+
+# print(pow(15,2))
+
+"""
+Comparison Operators
+
+1) >
+2) <
+3) >=
+4) <=
+5) ==
+6) !=
+"""
+
+# a = 15
+# b = 10
+
+# print(a>b)
+# print(a==b)
+# print(a!=b)
+
+"""
+User Input
+----------------
+Input function --->by default stored in string 
+"""
+
+# print("Enter Number:")
+# a = int(input())
+
+# print(type(a))
+# b = int(input("Enter Number: "))
+
+# add = a + b
+
+# print("Addition: ",add)
+
+"""
+2 + 3 = 5 
+"""
+"""
+Syntax:
+----------------
+if condition:
+    //Operations
+    //statement 1
+    //
+elif condition:
+    //Operations
+else:
+    //Operations
+"""
+
+num1 = int(input("Enter Number 1:"))
+num2 = int(input("Enter Number 2:"))
+
+if num1 > num2:
+    print(num1,"is maximum")
+else:
+    print(num2,"is maximum") 
+
+"""
+Task:-->
+
+1) check two numbers are equal or not
+2) find odd and even numbers
+3)  A shop will give discount of 10% if the cost of purchased quantity is more than 1000.
+    # Ask user for quantity
+    # Assume each unit's cost is 100.
+    # Judge and print total cost for user. 
+
+4)  A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years. Ask user for their salary and year of service and print the net bonus amount.
+
+5)  A school has following rules for grading system:
+    # a. Below 25 - F
+    # b. 25 to 45 - E
+    # c. 45 to 50 - D
+    # d. 50 to 60 - C
+    # e. 60 to 80 - B
+    # f. Above 80 - A
+    # Ask user to enter marks and print the corresponding grade.
+
+6)  A student will not be allowed to sit in exam if his/her attendance is less than 75%.
+    # Take following input from user:
+    # Number of classes held
+    # Number of classes attended.
+    # And print:
+    # percentage of class attended
+    # Is student is allowed to sit in exam or not.
+
+7) Wap to find no. of month from the given no. of days
+   
+8) wap to scan seconds and print hour, minute and remaining seconds
+   
+9) wap to swap 3 numbers that is scanned by the user (a->b, b->c, c->a)
+   
+10) wap to find out the maximum, minimum, average of the numbers that is scanned by the user
+    
+
+"""
